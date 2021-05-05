e06e2c65592ace47ad373efadd056d4d309169fd
Willian Rodrigues Barbosa <willian.barbosa@gazin.com.br>
2021-05-05 17:12:42 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f1936e8bd8527bae67de3919f0167efd'),
+    },
+  },
+});
